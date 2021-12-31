4c5fb712665716e5c3018b82505eb2a94c29b0a3
Baiju Dodhia <51258945+baijudodhia@users.noreply.github.com>
2021-12-31 16:14:59 +0530

    Update: Set default theme based on system theme & then localStorage.

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -1,31 +1,39 @@
 function changeTheme() {
     var theme = document.getElementById("header-theme-changer-btn").getAttribute("theme-value");
     if (theme === "dark") {
-        document.getElementById("header-theme-changer-btn").setAttribute("theme-value", "light");
-        document.getElementById("header-theme-changer-btn").value = "🌑";
-        document.getElementsByTagName("html")[0].setAttribute("data-theme", "light");
+        setLightTheme();
         localStorage.setItem("theme", "light");
     } else {
-        document.getElementById("header-theme-changer-btn").setAttribute("theme-value", "dark");
-        document.getElementById("header-theme-changer-btn").value = "🌕";
-        document.getElementsByTagName("html")[0].setAttribute("data-theme", "dark");
+        setDarkTheme();
         localStorage.setItem("theme", "dark");
     }
 }
 function setDefaultTheme() {
-    if(localStorage.getItem("theme") !== null) {
+    if (localStorage.getItem("theme") !== null) {
         let theme = localStorage.getItem("theme");
         if (theme === "light") {
-            document.getElementById("header-theme-changer-btn").setAttribute("theme-value", "light");
-            document.getElementById("header-theme-changer-btn").value = "🌑";
-            document.getElementsByTagName("html")[0].setAttribute("data-theme", "light");
+            setLightTheme();
         } else {
-            document.getElementById("header-theme-changer-btn").setAttribute("theme-value", "dark");
-            document.getElementById("header-theme-changer-btn").value = "🌕";
-            document.getElementsByTagName("html")[0].setAttribute("data-theme", "dark");
+            setDarkTheme();
         }
+    } else if (window.matchMedia("(prefers-color-scheme:light)").matches) {
+        setLightTheme();
+        localStorage.setItem("theme", 'light');
+    } else if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
+        setDarkTheme();
+        localStorage.setItem("theme", 'dark');
     } else {
         var theme = document.getElementById("header-theme-changer-btn").getAttribute("theme-value");
         localStorage.setItem("theme", theme);
     }
+}
+function setDarkTheme() {
+    document.getElementById("header-theme-changer-btn").setAttribute("theme-value", "dark");
+    document.getElementById("header-theme-changer-btn").value = "🌕";
+    document.getElementsByTagName("html")[0].setAttribute("data-theme", "dark");
+}
+function setLightTheme() {
+    document.getElementById("header-theme-changer-btn").setAttribute("theme-value", "light");
+    document.getElementById("header-theme-changer-btn").value = "🌑";
+    document.getElementsByTagName("html")[0].setAttribute("data-theme", "light");
 }
